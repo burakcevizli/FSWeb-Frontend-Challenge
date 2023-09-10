2670619aa5754c5a7e31745e0b28682758835df6
KrieqqeR <burakcevizli@gmail.com>
2023-09-10 21:36:29 +0300

    Updated.

--- web-sayfam/src/layouts/Header.js
+++ web-sayfam/src/layouts/Header.js
@@ -1,51 +1,68 @@
 import React from 'react'
-import "../layouts/HeaderCss.css"
 import githubLigth from "../assets/github-lighth.png"
 import LinkedinLight from "../assets/LinkedIn.png"
 import HeroPicture from "../assets/resim.png"
 
 export default function Header() {
   return (
-    <header className='flex HeaderClass '>
-      <div className='HeaderPurpleDiv'>
-
-        <p className='turkceIngılızceGecis cursor-pointer font-bold'><span className='Türkçe'>TÜRKÇE</span><span className='Geçiş'>'YE GEÇ</span></p>
+    <>
+      <div className="header flex">
+        <div className="header-left bg-purple min-w-[70%]">
+          <p className="text-green text-xl text-right mt-8 cursor-pointer mr-8"><span className='text-green'>TÜRKÇE</span> <span style={{color:"#D9D9D9"}}>'YE GEÇ</span></p>
+          <p className="text-green text-3xl ml-72">BURAK</p>
+        </div>
 
         
-          <div className='SolPaddingHeader '>
-            <h1 className="almila absolute top-12 font-bold">almila</h1>
-            <h1 className='text-5xl text-green font-bold'>I am frontend developer...</h1>
-            <p className='text-xl text-white '>...who likes to craft solid and scalable frontend products with great user experiences.</p>
-            <div className='buttons mt-7'>
-              <button className='bg-white flex gap-2'>
-                <img src={githubLigth} alt='github-light'></img>
-                <p className='text-center'>Github</p>
-              </button>
-              <button className='bg-white flex gap-2'>
-                <img src={LinkedinLight} alt='LinkedinLight'></img>
-                <p>Linkedin</p>
-              </button>
+        <div className="header-right bg-green min-w-[30%]">
+          <div className='DARKMODE bg-green'>
+            <div className="flex flex-row gap-2 w-full items-center mt-8 ml-8 cursor-pointer" id="NightModeRoot">
+              <div className="bg-[#8f88ff] mt-0.5 self-start flex flex-row justify-end w-12 shrink-0 h-6 items-center px-1 rounded-[100px]">
+                <div className="bg-[#ffe86e] w-4 shrink-0 h-4 rounded-[50%]" id="Ellipse" />
+                <div
+                  className="bg-[#e92577] w-px shrink-0 h-px rounded-[50%]"
+                  id="Ellipse1"
+                />
+              </div>
+              <div className="font-['Inter'] tracking-[1.5] text-[#4731d3] text-xl ">
+                DARK MODE
+              </div>
             </div>
-            <img src={HeroPicture} alt='hero' style={{ width: "31.25rem" }} className='resim ' />
-          </div>
-      
-
-
-      </div>
-      <div className='DARKMODE bg-green'>
-        <div className="flex flex-row gap-2 w-full items-center mt-8 ml-8 cursor-pointer" id="NightModeRoot">
-          <div className="bg-[#8f88ff] self-start flex flex-row justify-end w-12 shrink-0 h-6 items-center px-1 rounded-[100px]">
-            <div className="bg-[#ffe86e] w-4 shrink-0 h-4 rounded-[50%]" id="Ellipse" />
-            <div
-              className="bg-[#e92577] w-px shrink-0 h-px rounded-[50%]"
-              id="Ellipse1"
-            />
-          </div>
-          <div className="text-sm font-['Inter'] tracking-[1.5] text-[#4731d3] font-bold">
-            DARK MODE
           </div>
         </div>
       </div>
-    </header>
+
+      <div className="hero-content flex">
+        <div className="hero-left bg-purple min-w-[70%] self-end ">
+          <p className="text-green text-6xl ml-72 mr-48 mt-24">
+            I am a Frontend
+          </p>
+          <p className="text-green text-6xl ml-72 mr-48">Developer...</p>
+          <p className="text-white text-2xl ml-72 mr-48 mt-12">
+            ...who likes to craft solid and scalable frontend products whith
+            great user experiences
+          </p>
+          <div className='buttons gap-4 flex ml-72 mb-24 mt-4'>
+            <button className='bg-white rounded-md py-4 px-8 flex gap-2'>
+              <img src={githubLigth} alt='github-light'></img>
+              <p className='text-center'>Github</p>
+            </button>
+            <button className='bg-white rounded-md py-4 px-8 flex gap-2'>
+              <img src={LinkedinLight} alt='LinkedinLight'></img>
+              <p className='mt-1'>Linkedin</p>
+            </button>
+          </div>
+        </div>
+
+        <div className="hero-right bg-green min-w-[30%]">
+          <img
+            className="-ml-64 absolute"
+            src={HeroPicture}
+            alt="heroright"
+          ></img>
+        </div>
+      </div>
+
+    </>
+
   )
 }
--- web-sayfam/src/layouts/Skills.js
+++ web-sayfam/src/layouts/Skills.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import "../layouts/SkillsCss.css"
 import JavascriptLogo from "../assets/js.png"
 import ReactLogo from "../assets/react.png"
 import ReduxLogo from "../assets/redux.png"
@@ -8,17 +7,23 @@ import VsCodeLogo from "../assets/vsCode.png"
 import FigmaLogo from "../assets/figma.png"
 
 export default function Skills() {
-    const array = [
-        JavascriptLogo, ReactLogo, ReduxLogo, NodeLogo, VsCodeLogo, FigmaLogo
+    const array1 = [
+        JavascriptLogo, ReactLogo, ReduxLogo
+    ]
+    const array2 = [
+        NodeLogo, VsCodeLogo, FigmaLogo
     ]
     return (
-        <div className='skillsMain'>
-            <p className='Skills text-8xl'>Skills</p>
-            <div className='SecondMain' style={{ width: "60%" }}>
-
-
+        <div className=' w-8/12 ml-72 flex justify-between'>
+            <p className=' text-8xl text-[#4832D3]' >Skills</p>
+            <div className='flex justify-between'>
                 <div className='ImagesDiv flex flex-wrap'>
-                    {array.map(item =>
+                    {array1.map(item =>
+                        <img className='images' src={item} alt='Pictures' />
+                    )}
+                </div>
+                <div className='ImagesDiv flex flex-wrap'>
+                    {array2.map(item =>
                         <img className='images' src={item} alt='Pictures' />
                     )}
                 </div>
