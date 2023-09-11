edbced19385508ce72eb79ab5c6a1f61b10e3cfe
KrieqqeR <burakcevizli@gmail.com>
2023-09-11 18:02:48 +0300

    Updated.

--- web-sayfam/src/layouts/Footer.js
+++ web-sayfam/src/layouts/Footer.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useContext } from 'react'
+import React, { useContext } from 'react'
 import Mail from "../assets/mail.png"
 import Twitter from "../assets/twitter.png"
 import Codepen from "../assets/codepen.png"
