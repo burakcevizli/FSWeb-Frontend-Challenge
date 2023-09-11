58b083a385d75ab25ad590da3b22c4f058bf5cc0
KrieqqeR <burakcevizli@gmail.com>
2023-09-11 11:36:14 +0300

    Updating.

--- web-sayfam/src/layouts/Skills.js
+++ web-sayfam/src/layouts/Skills.js
@@ -8,25 +8,29 @@ import FigmaLogo from "../assets/figma.png"
 
 export default function Skills() {
     const array1 = [
-        JavascriptLogo, ReactLogo, ReduxLogo
+        JavascriptLogo, ReactLogo, ReduxLogo,NodeLogo, VsCodeLogo, FigmaLogo
     ]
     const array2 = [
-        NodeLogo, VsCodeLogo, FigmaLogo
+        // NodeLogo, VsCodeLogo, FigmaLogo
     ]
     return (
         <div className=' w-8/12 ml-72 flex justify-between'>
             <p className=' text-8xl text-[#4832D3]' >Skills</p>
-            <div className='flex justify-between'>
-                <div className='ImagesDiv flex flex-col flex-wrap'>
+            <div className='flex justify-between gap-16'>
+                <div className='ImagesDiv flex flex-row max-h-74 flex-wrap gap-4'>
                     {array1.map(item =>
-                        <img className='images' src={item} alt='Pictures' />
+                        <div>
+                            <img className='images' src={item} alt='Pictures' />
+                        </div>
                     )}
                 </div>
-                <div className='ImagesDiv flex flex-col flex-wrap'>
+                {/* <div className='ImagesDiv flex flex-col flex-wrap gap-4'>
                     {array2.map(item =>
-                        <img className='images' src={item} alt='Pictures' />
+                        <div>
+                            <img className='images' src={item} alt='Pictures' />
+                        </div>
                     )}
-                </div>
+                </div> */}
             </div>
         </div>
 
