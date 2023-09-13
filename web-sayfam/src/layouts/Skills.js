49f0abcad1f7980e7ca44b94370748be8538ca2c
KrieqqeR <burakcevizli@gmail.com>
2023-09-13 16:25:43 +0300

    asd

--- web-sayfam/src/layouts/Skills.js
+++ web-sayfam/src/layouts/Skills.js
@@ -11,14 +11,14 @@ export default function Skills() {
         <div className='dark:bg-[#252128;]'>
             <div className=' max-w-[66%] ml-80 flex justify-between py-24 '>
                 <p className=' text-6xl text-[#4832D3] dark:text-[#CBF281]' >{data[language].skills.skils}</p>
-                <div className='flex flex-col gap-12' style={{ maxHeight: "50vh" }}>
+                <div className='flex flex-col gap-12' style={{ height: "60%" }}>
                     {(darkMode ? data[language].skills.skillsDarkArray1 : data[language].skills.skillsArray1).map((item, index) =>
                         <div key={index}>
                             <img src={item} alt='Pictures' />
                         </div>
                     )}
                 </div>
-                <div className='flex flex-col gap-12' style={{ maxHeight: "50vh" }}>
+                <div className='flex flex-col gap-12' style={{ maxHeight: "60vh" }}>
                     {(darkMode ? data[language].skills.skillsDarkArray2 : data[language].skills.skillsArray2).map((item, index) =>
                         <div key={index}>
                             <img src={item} alt='Pictures' />
